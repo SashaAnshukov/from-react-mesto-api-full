3cf631d1602049727aaf73a2ef4a78ab910d43a3
SashaAnshukov <buenosdias2@yandex.ru>
2022-03-02 12:53:01 +0300

    1.50st rewiew fix errors

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,6 +3,25 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 const auth = (req, res, next) => {
+  const authorization = req.headers;
+  if (!authorization || authorization.startWith('Bearer')){
+    res.status(401).send({message: 'Прошляпил авторизацию'})
+  }
+  const token = authorization.replace('Bearer', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация ёпт'));
+  }
+
+  req.user = payload;
+
+  return next();
+};
+
+/*const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   // const token = req.headers.cookie;
   // console.log(req.headers.cookie);
@@ -17,6 +36,6 @@ const auth = (req, res, next) => {
   req.user = payload;
 
   next();
-};
+};*/
 
 module.exports = auth;
