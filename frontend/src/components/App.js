6fef5d9720473a4b2be34e0ce6ced34bd7a42fb6
SashaAnshukov <buenosdias2@yandex.ru>
2022-03-03 22:53:40 +0300

    1.108st rewiew fix errors

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -90,7 +90,7 @@ function App() {
   function handleCardLike(card) {
     
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api.сhangeLikeCard(card._id, isLiked).then((newCard) => {
       setCards((state) => state.map((c) => c._id === card._id ? newCard : c));
