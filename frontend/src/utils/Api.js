e2e22c6f6097a2ed8b0b170d5d002853387380aa
SashaAnshukov <buenosdias2@yandex.ru>
2022-09-10 12:11:51 +0300

    change PORT number in a package.json

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -118,7 +118,7 @@ export class Api {
 }
 
 const api = new Api({
-    adress: 'https://from-react-mesto-api-full-b.vercel.app',
+    adress: 'https://buenosdias2.nomoredomains.work',
     //token : '86724e9f-206a-43a9-ab92-a5e8d301d078'
 })
 
